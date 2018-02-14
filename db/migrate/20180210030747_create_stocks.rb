30dee5e70e4e531528d019cc4e06463dea302d16
Jean Malan <jeanmalan22@gmail.com>
2018-02-14 19:15:21 +0000

    Final

--- db/migrate/20180210030747_create_stocks.rb
+++ db/migrate/20180210030747_create_stocks.rb
@@ -3,10 +3,10 @@ class CreateStocks < ActiveRecord::Migration
     create_table :stocks do |t|
       t.string :ticker
       t.integer :user_id
-      t.integer :quantity
+
 
       t.timestamps null: false
     end
-    add_index :stocks, :user_id, :quantity
+    add_index :stocks, :user_id
   end
 end
